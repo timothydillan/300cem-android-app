085c88c7edf0236cd936306d8858882fb519439a
Timothy Dillan <55008869+timothydillan@users.noreply.github.com>
2021-02-03 21:15:48 +0800

    Change method parameter to use lambda instead

--- app/src/main/java/com/timothydillan/circles/SplashActivity.java
+++ app/src/main/java/com/timothydillan/circles/SplashActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.timothydillan.circles.Utils.CircleUtil;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -18,13 +20,10 @@ public class SplashActivity extends AppCompatActivity {
         // In here, we override the run method that belongs to the Runnable object and instead load the Main Activity
         // after 3 seconds.
         final int SPLASH_SCREEN_DURATION = 3000;
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent mainActivity = new Intent(SplashActivity.this, SignUpActivity.class);
-                startActivity(mainActivity);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent mainActivity = new Intent(SplashActivity.this, SignUpActivity.class);
+            startActivity(mainActivity);
+            finish();
         }, SPLASH_SCREEN_DURATION);
 
     }
