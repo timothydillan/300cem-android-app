2f5811039b1c77a14297e574ecdde84e823b0fcb
Timothy Dillan <55008869+timothydillan@users.noreply.github.com>
2021-02-10 19:32:29 +0800

    Add StorageReference to FirebaseUtil

--- app/src/main/java/com/timothydillan/circles/Utils/FirebaseUtil.java
+++ app/src/main/java/com/timothydillan/circles/Utils/FirebaseUtil.java
@@ -1,26 +1,25 @@
 package com.timothydillan.circles.Utils;
 
-import android.os.Handler;
-import android.util.Log;
-
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.timothydillan.circles.UI.ProgressButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class FirebaseUtil {
+    private static StorageReference firebaseStorageReference;
     private static DatabaseReference firebaseDatabaseReference = null;
     private static FirebaseAuth firebaseAuth = null;
     private static FirebaseUser firebaseCurrentUser = null;
 
-    public static void initializeFirebaseDbAndAuth() {
-        if (firebaseDatabaseReference != null && firebaseAuth != null) {
+    public static void initializeFirebaseDbAuthStorage() {
+        if (firebaseDatabaseReference != null && firebaseAuth != null && firebaseStorageReference != null) {
             return;
         }
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseStorageReference = FirebaseStorage.getInstance().getReference();
     }
 
     public static void initializeCurrentFirebaseUser() {
@@ -42,9 +41,10 @@ public class FirebaseUtil {
         return firebaseAuth;
     }
 
+    public static StorageReference getStorageReference() { return firebaseStorageReference; }
+
     public static void signOut() {
         firebaseCurrentUser = null;
         firebaseAuth.signOut();
     }
-
 }
