d9cf93bdfcfdacbc2cf49ab96e00545a3dbb1dbe
timothydillan <timothydillan@users.noreply.github.com>
2021-02-17 12:46:58 +0800

    Add new variables to User data for health

--- app/src/main/java/com/timothydillan/circles/Models/User.java
+++ app/src/main/java/com/timothydillan/circles/Models/User.java
@@ -1,26 +1,28 @@
 package com.timothydillan.circles.Models;
 
-import com.google.android.gms.maps.model.LatLng;
+import com.timothydillan.circles.Utils.HealthUtil;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     public String firstName;
     public String lastName;
     public String email;
     public String phone = "";
+    public String gender = "";
+    public String birthDate = "";
+    public String profilePicUrl = "";
+    public String token;
     public int currentCircleSession;
     public double latitude;
     public double longitude;
     public String lastSharingTime;
-    public String gender = "";
-    public String birthDate = "";
     public String uid;
-    public String profilePicUrl = "";
-    public String token = "";
+    public String heartRate;
+    public HashMap<String, String> stepCount;
 
     public User() {
 
@@ -33,7 +35,7 @@ public class User {
         this.email = email;
         this.phone = phone;
         this.currentCircleSession = currentCircleSession;
-        this.lastSharingTime = getCurrentDateAndTime();
+        this.lastSharingTime = getCurrentDayAndTime();
         this.token = token;
     }
 
@@ -43,8 +45,30 @@ public class User {
         this.lastName = lastName;
         this.email = email;
         this.currentCircleSession = currentCircleSession;
-        this.lastSharingTime = getCurrentDateAndTime();
+        this.lastSharingTime = getCurrentDayAndTime();
         this.token = token;
+    }
+
+    public String getHeartRate() {
+        return heartRate;
+    }
+
+    public HashMap<String, String> getStepCount() {
+        return stepCount;
+    }
+
+    public void setHeartRate(String heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public void setStepCount(HashMap<String, String> stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public void setSteps(String newStepCount) {
+        HashMap<String, String> step = new HashMap<>();
+        step.put(HealthUtil.getDate(new Date()), String.valueOf(newStepCount));
+        this.stepCount = step;
     }
 
     public String getLastName() {
@@ -73,9 +97,9 @@ public class User {
 
     public String getLastSharingTime() { return lastSharingTime; }
 
-    public void updateLastSharingTime() { this.lastSharingTime = getCurrentDateAndTime(); }
+    public void updateLastSharingTime() { this.lastSharingTime = getCurrentDayAndTime(); }
 
-    public String getCurrentDateAndTime() {
+    public String getCurrentDayAndTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH.mm EEEE");
         return dateFormat.format(new Date());
     }
